e5477b57147667640bde65393f503aa92a9a57e6
Jonathan Rouleau <jonathan@rouleau.io>
2019-04-01 22:48:18 -0600

    Remove unused functions

--- src/utils/timing.js
+++ src/utils/timing.js
@@ -2,20 +2,6 @@
 
 const { GLib } = imports.gi;
 
-/* exported setTimeout */
-function setTimeout(func, millis) {
-  return GLib.timeout_add(GLib.PRIORITY_DEFAULT, millis, () => {
-    func();
-    // Don't repeat
-    return false;
-  });
-}
-
-/* exported clearTimeout */
-function clearTimeout(id) {
-  return GLib.Source.remove(id);
-}
-
 /* exported setInterval */
 function setInterval(func, millis) {
   return GLib.timeout_add(GLib.PRIORITY_DEFAULT, millis, () => {
@@ -28,19 +14,4 @@ function setInterval(func, millis) {
 /* exported clearInterval */
 function clearInterval(id) {
   return GLib.Source.remove(id);
-}
-
-/* exported debounce */
-function debounce(func, millis) {
-  let timer = null;
-  return function debounceInner(...args) {
-    const context = this;
-    if (timer !== null) {
-      clearTimeout(timer);
-    }
-    timer = setTimeout(() => {
-      func.apply(context, args);
-      timer = null;
-    }, millis);
-  };
 }
