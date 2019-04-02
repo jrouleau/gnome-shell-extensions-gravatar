8b92dc2ffccd263322582b764c8d3533858e1709
Jonathan Rouleau <jonathan@rouleau.io>
2019-04-01 19:18:26 -0600

    Rebuild the prefs widget
    * Fix invalid email handling
    * Add links to github on the About page
    * Prevent excessively downloading new images while the user is
      changing their email address by only saving the email setting
      when the prefs widget is closed

--- src/prefs.js
+++ src/prefs.js
@@ -1,13 +1,12 @@
 'use strict';
 
 const ExtensionUtils = imports.misc.extensionUtils;
-const Gdk = imports.gi.Gdk;
 const Gtk = imports.gi.Gtk;
 const Lang = imports.lang;
 
 const Me = ExtensionUtils.getCurrentExtension();
 const Convenience = Me.imports.lib.convenience;
-const { log, debug } = Me.imports.utils.log;
+const { debug } = Me.imports.utils.log;
 const { isValidEmail } = Me.imports.utils.isValidEmail;
 
 
@@ -23,34 +22,36 @@ const Prefs = new Lang.Class({
     this._rtl = Gtk.Widget.get_default_direction() === Gtk.TextDirection.RTL;
     this._settings = Convenience.getSettings();
 
-    // CSS Style
-    let cssProvider = new Gtk.CssProvider();
-    cssProvider.load_from_path(Me.path + '/prefs.css');
-    Gtk.StyleContext.add_provider_for_screen(
-      Gdk.Screen.get_default(),
-      cssProvider,
-      Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
-    );
-
-    // GtkBuilder
+    // Initialize GtkBuilder
     this._builder = new Gtk.Builder();
     this._builder.set_translation_domain(Me.metadata['gettext-domain']);
     this._builder.add_from_file(Me.path + '/prefs.ui');
 
-    // Widget
-    this._widget = this._builder.get_object('prefs_notebook');
-
-    // Display current preferences
-    this._loadPrefs();
-
-    // Connect signals to handler
-    this._builder.connect_signals_full(function (builder, object, signal, handler) {
-      if (this._SignalHandler[handler]) {
-        object.connect(signal, this._SignalHandler[handler].bind(this));
-      } else {
-        log('Unknown signal: ' + handler);
+    // Setup the prefs widget
+    this._widget = this._builder.get_object('prefs_widget');
+    this._widget.connect('destroy', Lang.bind(this, function () {
+      if (this._email !== this._settings.get_string('email')) {
+        debug('Updating email to "' + this._email + '"');
+        this._settings.set_string('email', this._email);
       }
-    }.bind(this));
+    }));
+
+    // Setup the email settings entry
+    let email_obj = this._builder.get_object('email');
+    email_obj.connect('changed', Lang.bind(this, function (obj) {
+      this._email = obj.get_text().trim();
+
+      if (isValidEmail(this._email)) {
+        obj.set_icon_from_icon_name(Gtk.PositionType.RIGHT, null);
+      } else {
+        obj.set_icon_from_icon_name(Gtk.PositionType.RIGHT, 'dialog-warning');
+      }
+    }));
+    email_obj.set_text(this._settings.get_string('email'));
+
+    // Set the version text
+    let version_obj = this._builder.get_object('version');
+    version_obj.set_text(Me.metadata.version.toString());
   },
 
   /*
@@ -60,48 +61,6 @@ const Prefs = new Lang.Class({
    */
   getWidget: function () {
     return this._widget;
-  },
-
-  /*
-   ***********************************************
-   * Private Methods                             *
-   ***********************************************
-   */
-  _loadPrefs: function () {
-    // email_entry
-    let email_entry = this._builder.get_object('email_entry');
-    email_entry.set_text(this._settings.get_string('email'));
-
-    // version
-    let version = this._builder.get_object('version');
-    version.set_text(Me.metadata.version.toString());
-  },
-
-
-  _SignalHandler: {
-    email_entry_activate_cb: function (entry) {
-      // Update email address
-      let email = entry.get_text().trim();
-      let style = entry.get_style_context();
-      style.remove_class('valid');
-      if (isValidEmail(email) && email !== this._settings.get_string('email')) {
-        debug('Updating email');
-        this._settings.set_string('email', email);
-      }
-    },
-
-    email_entry_changed_cb: function (entry) {
-      // Validate email address
-      let email = entry.get_text().trim();
-      let style = entry.get_style_context();
-      if (isValidEmail(email)) {
-        style.remove_class('invalid');
-        style.add_class('valid');
-      } else {
-        style.remove_class('valid');
-        style.add_class('invalid');
-      }
-    },
   },
 
 });
